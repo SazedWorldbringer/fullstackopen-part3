891ccde69bce5f6fbf91afe926d8fff29f66cfa5
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-05 09:48:10 +0530

    feat: install and use cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 morgan.token('data', function(req, res) { return JSON.stringify(req.body) })
 
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
 
 let persons = [
 	{
